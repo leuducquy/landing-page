c1818fedaabc1db904c163f19a9192f78925a695
Quy Leu Duc <quy.leu@kyanon.digital>
2023-11-09 10:47:14 +0700

    adđ transcomponent

--- src/components/ContentBlock/RightContentBlock/index.tsx
+++ src/components/ContentBlock/RightContentBlock/index.tsx
@@ -4,6 +4,8 @@ import { SvgIcon } from "../../../common/SvgIcon";
 import { Button } from "../../../common/Button";
 import { ContentBlockProps } from "../types";
 import { Fade } from "react-awesome-reveal";
+import { Trans } from "react-i18next";
+
 import {
   RightBlockContainer,
   Content,
@@ -32,7 +34,7 @@ const RightBlock = ({
           <Col lg={11} md={11} sm={11} xs={24}>
             <ContentWrapper>
               <h6>{t(title)}</h6>
-              <Content>{t(content)}</Content>
+              <Content><Trans i18nKey={t(content)} components={{ 1: <br /> }} /></Content>
               <ButtonWrapper>
                 {typeof button === "object" &&
                   button.map((item: any, id: number) => {
--- src/components/ContentBlock/RightContentBlock/styles.ts
+++ src/components/ContentBlock/RightContentBlock/styles.ts
@@ -15,6 +15,7 @@ export const RightBlockContainer = styled("section")`
 
 export const Content = styled("p")`
   margin: 1.5rem 0 2rem 0;
+  white-space: pre-line;
 `;
 
 export const ContentWrapper = styled("div")`

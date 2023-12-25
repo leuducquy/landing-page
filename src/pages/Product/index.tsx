eb0f430f38ed03cf26d9a95b6a0d072b70ad5fc1
Quy Leu Duc <quy.leu@kyanon.digital>
2023-12-25 14:28:38 +0700

    commit : modify watch store

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -4,6 +4,7 @@ import beachImage from "../../img/borabora1.jpg"
 import gymImage from "../../img/gym.jpeg"
 import shoesImage from "../../img/hero-img.png"
 import foofImage from "../../img/foodimage.avif"
+import watchImage from "../../img/watch.jpeg"
 import './product.css'
 // Sample data for the card list
 const cardData = [
@@ -34,6 +35,13 @@ const cardData = [
     description: 'Join us on virtual food adventures as we explore bustling markets, hidden eateries, and iconic restaurants. Our immersive videos and articles bring the sights, sounds, and, most importantly, the tastes directly to you.',
     imageUrl: foofImage,
     url:"https://foodlooking-leuducquys-projects.vercel.app"
+  },
+  {
+    id: 5,
+    title: 'A Watch Store Classic ',
+    description: 'Step into a world where craftsmanship meets sophistication. Our watch store, a timeless classic, invites you to explore an exquisite collection of watches that transcend trends. From meticulously designed timepieces to iconic classics, immerse yourself in a journey through the ages of horology. Discover the perfect blend of style and functionality, where each watch tells a story of precision and elegance. Elevate your wristwear with our curated selection, where every tick echoes the legacy of timeless design. Welcome to a watch store classic, where time takes on a new dimension.',
+    imageUrl: watchImage,
+    url:"https://watch-leuducquys-projects.vercel.app"
   },
 ];
 const Product = () => {
